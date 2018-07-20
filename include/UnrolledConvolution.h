876c846910f0d1046583fd9954399885a8b3b07d
Ofer Dekel <oferd@microsoft.com>
2018-07-20 00:52:48 -0700

    added chlmajor unrolled convolution

--- include/UnrolledConvolution.h
+++ include/UnrolledConvolution.h
@@ -13,7 +13,7 @@
 #include <vector> // std::vector
 
 template <typename ElementType>
-void UnrollInput(const ElementType* XRowMaj, ElementType* U, int wRows, int wCols, int wChls, int vStride, int hStride, int yRows, int yCols)
+void UnrollInputRowMaj(const ElementType* XRowMaj, ElementType* URowMaj, int wRows, int wCols, int wChls, int vStride, int hStride, int yRows, int yCols)
 {
     int xCols = hStride * (yCols - 1) + wCols;
     int xChls = wChls;
@@ -27,7 +27,7 @@ void UnrollInput(const ElementType* XRowMaj, ElementType* U, int wRows, int wCol
             {
                 // calculate memcpy target
                 int uRow = yRow * yCols + yCol;
-                float* target = U + (uRow * wRows + wRow) * copySize;
+                float* target = URowMaj + (uRow * wRows + wRow) * copySize;
                 
                 // calculate memcpy source
                 int xRow = yRow * vStride + wRow;
@@ -42,7 +42,7 @@ void UnrollInput(const ElementType* XRowMaj, ElementType* U, int wRows, int wCol
 }
 
 template <typename ElementType>
-void UnrolledConvolution(const ElementType* WChlMaj, const ElementType* XRowMaj, ElementType* YRowMaj, int wCount, int wRows, int wCols, int wChls, int vStride, int hStride, int yRows, int yCols)
+void UnrolledConvolutionRowMaj(const ElementType* WChlMaj, const ElementType* XRowMaj, ElementType* YRowMaj, int wCount, int wRows, int wCols, int wChls, int vStride, int hStride, int yRows, int yCols)
 {
     int yChls = wCount;
     int xRows = (yRows - 1) * vStride + wRows;
@@ -56,6 +56,57 @@ void UnrolledConvolution(const ElementType* WChlMaj, const ElementType* XRowMaj,
     ElementType* ZRowMaj = YRowMaj;
 
     std::vector<ElementType> URowMaj(uRows * uCols);
-    UnrollInput(XRowMaj, URowMaj.data(), wRows, wCols, wChls, vStride, hStride, yRows, yCols);
+    UnrollInputRowMaj(XRowMaj, URowMaj.data(), wRows, wCols, wChls, vStride, hStride, yRows, yCols);
     Gemm(true, false, true, uRows, vCols, uCols, 1, URowMaj.data(), VColMaj, 1, ZRowMaj);
+}
+
+template <typename ElementType>
+void UnrollInputChlMaj(const ElementType* XChlMaj, ElementType* UColMaj, int wRows, int wCols, int wChls, int vStride, int hStride, int yRows, int yCols)
+{
+    assert(hStride == 1);
+
+    int xRows = vStride * (yRows - 1) + wRows;
+    int xCols = yCols + wCols - 1;
+    int copySize = yCols;
+
+    for(int wRow = 0; wRow < wRows; ++wRow) {
+        for(int wCol = 0; wCol < wCols; ++wCol) {
+            for(int wChl = 0; wChl < wChls; ++wChl) {
+                for(int yRow = 0; yRow < yRows; ++yRow) {
+
+                    // calculate memcpy target
+                    int uCol =  (wRow * wCols + wCol) * wChls + wChl;
+                    ElementType* target = UColMaj + (uCol * yRows + yRow) * yCols;
+
+                    // calculate memcpy source
+                    int xRow = yRow * vStride + wRow;
+                    int xCol = wCol;
+                    int xChl = wChl;
+                    const float* source = XChlMaj + (xChl * xRows + xRow) * xCols + xCol;
+                    
+                    // copy from X to U
+                    memcpy(target, source, copySize * sizeof(ElementType));
+                }   
+            }  
+        }   
+    }   
+}
+
+template <typename ElementType>
+void UnrolledConvolutionChlMaj(const ElementType* WChlMaj, const ElementType* XChlMaj, ElementType* YRowMaj, int wCount, int wRows, int wCols, int wChls, int vStride, int hStride, int yRows, int yCols)
+{
+    int yChls = wCount;
+    int xRows = (yRows - 1) * vStride + wRows;
+    int xCols = (yCols - 1) * hStride + wCols;
+
+    int uRows = yRows * yCols;
+    int uCols = wRows * wCols * wChls;
+    int vCols = wCount;
+
+    const ElementType* VColMaj = WChlMaj;
+    ElementType* ZRowMaj = YRowMaj;
+
+    std::vector<ElementType> UColMaj(uRows * uCols);
+    UnrollInputChlMaj(XChlMaj, UColMaj.data(), wRows, wCols, wChls, vStride, hStride, yRows, yCols);
+    Gemm(false, false, true, uRows, vCols, uCols, 1, UColMaj.data(), VColMaj, 1, ZRowMaj);
 }
