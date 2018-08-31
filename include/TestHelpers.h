a6333929655c498d6b560db4d396d324877cb50f
Ofer Dekel <oferd@microsoft.com>
2018-08-31 00:54:35 -0700

    added timing code

--- include/TestHelpers.h
+++ include/TestHelpers.h
@@ -10,6 +10,7 @@
 #include "Tensor.h"
 
 #include <functional>
+#include <chrono>
 
 using TestType = std::function<Tensor<float,3>()>;
 
@@ -25,10 +26,22 @@ void RunTest(TestType test);
 template <typename ElementType, typename TensorType>
 double RunBenchmark(double testDuration, const std::vector<TensorType>& inputs, BenchmarkType<ElementType> benchmark)
 {
-    double time = 0.0;
-    for(const auto& input : inputs)
+    int repetitions = 0;
+    int duration = 0;
+    auto startTime = std::chrono::high_resolution_clock::now();
+    while (duration < testDuration)
     {
-        benchmark(input.Data());
+        for(const auto& input : inputs)
+        {
+            benchmark(input.Data());
+        }
+
+        auto currentTime = std::chrono::high_resolution_clock::now();
+        duration = (int)std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - startTime).count();
+
+        ++repetitions;
     }
-    return time;
+
+    auto millisecondsPerInput = static_cast<double>(duration) / (inputs.size() * repetitions);
+    return millisecondsPerInput;
 }
