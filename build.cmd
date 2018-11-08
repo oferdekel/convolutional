b76dc0759575e886ec8df4687f5c35bd161faec8
Ofer Dekel <oferd@microsoft.com>
2018-11-08 13:14:44 -0800

    updates to README

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 if not exist build_vs mkdir build_vs
 cd build_vs
-cmake -DBLAS_VERSION=winopenblas -G "Visual Studio 15 2017 Win64" ..
+cmake -DBLAS_VERSION=none -G "Visual Studio 15 2017 Win64" ..
 cmake --build . --config Release 
 cd ..
